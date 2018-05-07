e6094a6d96683bc328af1f3e2441018a9bdaac7a
miguelolivo <36650156+miguelolivo@users.noreply.github.com>
2018-05-07 09:39:35 -0400

    bcncbn
    bvhnn

--- CALL CENTER DEFINITIVO/Mantenimiento_Emp/Principal.vb
+++ CALL CENTER DEFINITIVO/Mantenimiento_Emp/Principal.vb
@@ -7,7 +7,7 @@ Public Class Principal
     Dim nomusu As String
     Dim nivel As String
 
-    '777'
+    '777droga'
 
     Private Sub show_mnu_Reg01(ByVal sender As Object, ByVal e As EventArgs) Handles mnu_Reg01.Click
         Try
